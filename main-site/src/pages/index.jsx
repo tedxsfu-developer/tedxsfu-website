68ad0387a32579dcd7962813d9472f02261d4e3c
Celine <hathungn614@gmail.com>
2022-05-07 14:54:59 -0700

    Fix styling for footer on small screen size

--- main-site/src/pages/index.jsx
+++ main-site/src/pages/index.jsx
@@ -11,7 +11,7 @@ const IndexPage = () => {
                 <h1 className="main-heading font-bold main-font">TEDxSFU 2022 Conference is coming soon!</h1>
                 <h2 className="secondary-heading font-light main-font">Stay tuned and check out <Link to={`/2021`} className="simple-link">TEDxSFU 2021 Conference</Link></h2>
             </div>
-            <footer className="flex space-between">
+            <footer>
                 <section className="social-media">
                     <p className="main-font">Connect with us</p>
                     <SocialMedia />
